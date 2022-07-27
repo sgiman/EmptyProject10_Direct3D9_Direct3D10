92fd981c83f4a6b7dd7b776334fd7888ace838b9
Sergey Gasanov <sgimancs@gmail.com>
2022-07-27 15:41:01 +0300

    test
    test

---
+++ EmptyProject10.cpp
@@ -0,0 +1,197 @@
+//***************************************************************************************
+// File: EmptyProject10.cpp
+//
+// Empty starting point for new Direct3D 9 and/or Direct3D 10 applications
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//--------------------------------------------------------------------------------------
+// Visual Studio 2010
+// Microsoft DirectX SDK (June 2010)
+//--------------------------------------------------------------------------------------
+// Modified by sgiman, 2022, jul
+//***************************************************************************************
+#include "DXUT.h"
+
+
+//--------------------------------------------------------------------------------------
+// Внешние объявления 
+//--------------------------------------------------------------------------------------
+extern bool CALLBACK IsD3D9DeviceAcceptable( D3DCAPS9* pCaps, D3DFORMAT AdapterFormat, D3DFORMAT BackBufferFormat,
+                                             bool bWindowed, void* pUserContext );
+extern HRESULT CALLBACK OnD3D9CreateDevice( IDirect3DDevice9* pd3dDevice,
+                                            const D3DSURFACE_DESC* pBackBufferSurfaceDesc, void* pUserContext );
+extern HRESULT CALLBACK OnD3D9ResetDevice( IDirect3DDevice9* pd3dDevice, const D3DSURFACE_DESC* pBackBufferSurfaceDesc,
+                                           void* pUserContext );
+extern void CALLBACK OnD3D9FrameRender( IDirect3DDevice9* pd3dDevice, double fTime, float fElapsedTime,
+                                        void* pUserContext );
+extern void CALLBACK OnD3D9LostDevice( void* pUserContext );
+extern void CALLBACK OnD3D9DestroyDevice( void* pUserContext );
+
+
+//--------------------------------------------------------------------------------------
+// Сбросить любые неприемлемые устройства D3D10, возвращая значение false.
+//--------------------------------------------------------------------------------------
+bool CALLBACK IsD3D10DeviceAcceptable( UINT Adapter, UINT Output, D3D10_DRIVER_TYPE DeviceType,
+                                       DXGI_FORMAT BackBufferFormat, bool bWindowed, void* pUserContext )
+{
+    return true;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Вызывается непосредственно перед созданием устройства D3D9 или D3D10, 
+// что позволяет приложению изменять настройки устройства по мере необходимости
+//--------------------------------------------------------------------------------------
+bool CALLBACK ModifyDeviceSettings( DXUTDeviceSettings* pDeviceSettings, void* pUserContext )
+{
+    return true;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Создать любые ресурсы D3D10, которые не зависят от заднего буфера
+//--------------------------------------------------------------------------------------
+HRESULT CALLBACK OnD3D10CreateDevice( ID3D10Device* pd3dDevice, const DXGI_SURFACE_DESC* pBackBufferSurfaceDesc,
+                                      void* pUserContext )
+{
+    return S_OK;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Создаем любые ресурсы D3D10, зависящие от заднего буфера
+//--------------------------------------------------------------------------------------
+HRESULT CALLBACK OnD3D10ResizedSwapChain( ID3D10Device* pd3dDevice, IDXGISwapChain* pSwapChain,
+                                          const DXGI_SURFACE_DESC* pBackBufferSurfaceDesc, void* pUserContext )
+{
+    return S_OK;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Обработка обновлений сцены. Это вызывается независимо от того, какой D3D API используется.
+//--------------------------------------------------------------------------------------
+void CALLBACK OnFrameMove( double fTime, float fElapsedTime, void* pUserContext )
+{
+}
+
+
+//--------------------------------------------------------------------------------------
+// Рендерить сцену с помощью устройства D3D10
+//--------------------------------------------------------------------------------------
+void CALLBACK OnD3D10FrameRender( ID3D10Device* pd3dDevice, double fTime, float fElapsedTime, void* pUserContext )
+{
+    // Clear render target and the depth stencil 
+    float ClearColor[4] = { 0.176f, 0.196f, 0.667f, 0.0f };
+    pd3dDevice->ClearRenderTargetView( DXUTGetD3D10RenderTargetView(), ClearColor );
+    pd3dDevice->ClearDepthStencilView( DXUTGetD3D10DepthStencilView(), D3D10_CLEAR_DEPTH, 1.0, 0 );
+}
+
+
+//--------------------------------------------------------------------------------------
+// Освободить ресурсы D3D10, созданные в OnD3D10ResizedSwapChain
+//--------------------------------------------------------------------------------------
+void CALLBACK OnD3D10ReleasingSwapChain( void* pUserContext )
+{
+}
+
+
+//--------------------------------------------------------------------------------------
+// Освободить ресурсы D3D10, созданные в OnD3D10CreateDevice
+//--------------------------------------------------------------------------------------
+void CALLBACK OnD3D10DestroyDevice( void* pUserContext )
+{
+}
+
+
+//--------------------------------------------------------------------------------------
+// Обработка сообщений в приложение
+//--------------------------------------------------------------------------------------
+LRESULT CALLBACK MsgProc( HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam,
+                          bool* pbNoFurtherProcessing, void* pUserContext )
+{
+    return 0;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Обработка нажатия клавиш
+//--------------------------------------------------------------------------------------
+void CALLBACK OnKeyboard( UINT nChar, bool bKeyDown, bool bAltDown, void* pUserContext )
+{
+}
+
+
+//--------------------------------------------------------------------------------------
+// Обработка нажатия кнопки мыши
+//--------------------------------------------------------------------------------------
+void CALLBACK OnMouse( bool bLeftButtonDown, bool bRightButtonDown, bool bMiddleButtonDown,
+                       bool bSideButton1Down, bool bSideButton2Down, int nMouseWheelDelta,
+                       int xPos, int yPos, void* pUserContext )
+{
+}
+
+
+//--------------------------------------------------------------------------------------
+// Вызов, если устройство было удалено. 
+// Верните true, чтобы найти новое устройство, и false, чтобы выйти.
+//--------------------------------------------------------------------------------------
+bool CALLBACK OnDeviceRemoved( void* pUserContext )
+{
+    return true;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Инициализируем все и переходим в цикл рендеринга
+//--------------------------------------------------------------------------------------
+int WINAPI wWinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow )
+{
+    // Включите проверку памяти во время выполнения для отладочных сборок.
+#if defined(DEBUG) | defined(_DEBUG)
+    _CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+#endif
+
+	// DXUT создаст и использует лучшее устройство (либо D3D9, либо D3D10)
+    // который доступен в системе в зависимости от того, какие обратные вызовы D3D установлены ниже
+
+    // Установить общие обратные вызовы DXUT
+    DXUTSetCallbackFrameMove( OnFrameMove );
+    DXUTSetCallbackKeyboard( OnKeyboard );
+    DXUTSetCallbackMouse( OnMouse );
+    DXUTSetCallbackMsgProc( MsgProc );
+    DXUTSetCallbackDeviceChanging( ModifyDeviceSettings );
+    DXUTSetCallbackDeviceRemoved( OnDeviceRemoved );
+
+    // Установить обратные вызовы D3D9 DXUT. 
+	// Удалить эти наборы, если приложению не требуется поддержка D3D9.
+    DXUTSetCallbackD3D9DeviceAcceptable( IsD3D9DeviceAcceptable );
+    DXUTSetCallbackD3D9DeviceCreated( OnD3D9CreateDevice );
+    DXUTSetCallbackD3D9DeviceReset( OnD3D9ResetDevice );
+    DXUTSetCallbackD3D9FrameRender( OnD3D9FrameRender );
+    DXUTSetCallbackD3D9DeviceLost( OnD3D9LostDevice );
+    DXUTSetCallbackD3D9DeviceDestroyed( OnD3D9DestroyDevice );
+
+	// Установить обратные вызовы D3D10 DXUT. 
+	// Удалить эти наборы, если приложению не требуется поддержка D3D10.
+    DXUTSetCallbackD3D10DeviceAcceptable( IsD3D10DeviceAcceptable );
+    DXUTSetCallbackD3D10DeviceCreated( OnD3D10CreateDevice );
+    DXUTSetCallbackD3D10SwapChainResized( OnD3D10ResizedSwapChain );
+    DXUTSetCallbackD3D10FrameRender( OnD3D10FrameRender );
+    DXUTSetCallbackD3D10SwapChainReleasing( OnD3D10ReleasingSwapChain );
+    DXUTSetCallbackD3D10DeviceDestroyed( OnD3D10DestroyDevice );
+
+	// Здесь выполняем любую инициализацию на уровне приложения
+
+    DXUTInit( true, true, NULL ); 			// Разобрать командную строку, показать msgbox при ошибке, без дополнительных параметров командной строки
+    DXUTSetCursorSettings( true, true ); 	// Показывать курсор и обрезать его в полноэкранном режиме
+    DXUTCreateWindow( L"EmptyProject10" );
+    DXUTCreateDevice( true, 640, 480 );
+    DXUTMainLoop(); 						// Войти в цикл рендеринга DXUT
+
+    // Выполните любую очистку на уровне приложения здесь
+
+    return DXUTGetExitCode();
+}
+
+
